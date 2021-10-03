d398ff5e881cdaaeec55b8d69d7c419eff393aae
sorin800 <sorintruica@gmail.com>
2021-10-03 12:06:52 +0300

    curs

---
+++ M01S02/ex01/shapes.js
@@ -0,0 +1,3 @@
+console.log(Math.pow(16, 2));
+console.log(20 * 16);
+console.log(15 * 2 + 38 * 2);
---
+++ M01S02/ex01/utils.js
@@ -0,0 +1,3 @@
+console.log(45 * 82);
+console.log(Math.pow(13, 2) + 2 * 13 * 41 + Math.pow(41, 2));
+console.log(Math.pow(15 / 2, 2) * Math.PI);
