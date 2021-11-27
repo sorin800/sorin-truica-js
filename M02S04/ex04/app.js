633885f62baff963dec4844108ef41c05f178a26
Sorin Truica <sorin.truica@gmail.com>
2021-11-27 17:03:49 +0200

    curs

--- M02S04/ex04/app.js
+++ M02S04/ex04/app.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('personForm');
-const submitButton = form.querySelector('button[type=submit]');
+const submitButton = form.querySelector('button[type="submit"]');
 
 const personForm = new Pristine(form);
-const isValid = personForm.validate();
+const isValid = personForm.validate(true);
 
 if (!isValid) {
   submitButton.disabled = true;
@@ -10,4 +10,17 @@ if (!isValid) {
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
+  // do Ajax stuff
+});
+
+form.addEventListener('input', (event) => {
+  const input = event.target;
+
+  if (input.name === undefined) {
+    return;
+  }
+
+  const isValid = personForm.validate(true);
+  submitButton.disabled = !isValid;
 });
